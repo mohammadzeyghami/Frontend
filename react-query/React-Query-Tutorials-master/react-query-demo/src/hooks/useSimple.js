f05e6b05bc2416289891ed0c24e29bdb5ac58039
mohammadzeyghami <mohammadzeighami197@gmail.com>
2023-08-24 17:47:10 +0330

    simple axios fetch

--- react-query/React-Query-Tutorials-master/react-query-demo/src/hooks/useSimple.js
+++ react-query/React-Query-Tutorials-master/react-query-demo/src/hooks/useSimple.js
@@ -1,17 +1,32 @@
-import { useQuery } from "react-query";
+// import { useQuery } from "react-query";
+// import axios from "axios";
+
+// const fetchDatas = () => {
+//   return axios.get("http://localhost:4000/superheros");
+// };
+
+// export const useSimple = (onError, onSuccess) => {
+//   return useQuery("simple", fetchDatas, {
+//     onSuccess,
+//     onError,
+//     select: (data) => {
+//       const superHeroNames = data.data.map((hero) => hero.name);
+//       return superHeroNames;
+//     },
+//   });
+// };
+
 import axios from "axios";
+import { useEffect, useState } from "react";
 
-const fetchDatas = () => {
-  return axios.get("http://localhost:4000/superheros");
+const useSimple = () => {
+  const [datas, setDatas] = useState("");
+  useEffect(() => {
+    axios
+      .get(`http://localhost:4000/superheros`)
+      .then((res) => console.log(res));
+  }, []);
+  return datas;
 };
 
-export const useSimple = (onError, onSuccess) => {
-  return useQuery("simple", fetchDatas, {
-    onSuccess,
-    onError,
-    select: (data) => {
-      const superHeroNames = data.data.map((hero) => hero.name);
-      return superHeroNames;
-    },
-  });
-};
+export default useSimple;
