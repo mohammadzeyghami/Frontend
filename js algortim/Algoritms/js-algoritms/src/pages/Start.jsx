bda7f09efe0e1830c99a2fd01ac523045030dd9a
mohammadzeyghami <mohammadzeighami197@gmail.com>
2023-09-22 18:51:45 +0330

    Big o notaion

--- js algortim/Algoritms/js-algoritms/src/pages/Start.jsx
+++ js algortim/Algoritms/js-algoritms/src/pages/Start.jsx
@@ -1,5 +1,22 @@
 const Start = () => {
-  return <div>start</div>;
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      {" "}
+      <h2>Big o Notaion</h2>
+      <p>be tedad tekrar har khat az function migam big o notaion</p>
+      <h2>Time Complexity</h2>
+      <p>O1 - Contant</p>
+      <p>O(n) - Linear</p>
+      <p>O(logn) - Logarithmic</p>
+    </div>
+  );
 };
 
 export default Start;
